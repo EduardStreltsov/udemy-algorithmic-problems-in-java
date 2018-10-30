93c623f500c3856907d4e404109a547ffaebad27
eduard.streltsov <streltsov.eduard@gmail.com>
2018-10-30 20:12:37 +0200

    lesson007

--- src/com/udemy/lesson007/EuclideanAlgorithm.java
+++ src/com/udemy/lesson007/EuclideanAlgorithm.java
@@ -2,12 +2,15 @@ package com.udemy.lesson007;
 
 public class EuclideanAlgorithm {
 	
-	public static int calculate(int n1, int n2) {
-		return 0;
+	public static int calculateRecursive(int n1, int n2) {
+		if (n2 == 0) return n1;
+		return calculateRecursive(n2, n1 % n2);
 	}
 	
 	public static void main(String[] args) {
-	
+		System.out.println(calculateRecursive(1,5));
+		System.out.println(calculateRecursive(6,9));
+		System.out.println(calculateRecursive(28,24));
 	}
 	
 }
