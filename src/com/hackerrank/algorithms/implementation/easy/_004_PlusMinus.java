64bf813fed9d5da43702212596b549c3e4fe2b90
eduard.streltsov <streltsov.eduard@gmail.com>
2019-03-05 19:20:29 +0100

    _004_PlusMinus

---
+++ src/com/hackerrank/algorithms/implementation/easy/_004_PlusMinus.java
@@ -0,0 +1,17 @@
+package com.hackerrank.algorithms.implementation.easy;
+
+import java.util.Arrays;
+
+public class _004_PlusMinus {
+	
+	private static void plusMinus(int[] arr) {
+		System.out.println((float) Arrays.stream(arr).filter(i -> i > 0).count() / arr.length);
+		System.out.println((float) Arrays.stream(arr).filter(i -> i < 0).count() / arr.length);
+		System.out.println((float) Arrays.stream(arr).filter(i -> i == 0).count() / arr.length);
+	}
+	
+	public static void main(String[] args) {
+		plusMinus(new int[]{-1, 1, -1, 0, 1, 1});
+	}
+	
+}
